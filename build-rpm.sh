6e1c72967463b015e26f0db8efb9176f20a7948c
tpgxyz <tpgxyz@gmail.com>
2018-02-09 00:39:31 +0100

    get rid of bashisms in build-rpm.sh

--- build-rpm.sh
+++ build-rpm.sh
@@ -410,7 +410,7 @@ validate_arch() {
 	local _PLATFORM=($1)
 # count for occurences
 	for item in "${SPEC_ARCH[@]}"; do
-	    if [[ "${_PLATFORM[@]}" =~ "${item}" ]] ; then
+	    if [[ "${_PLATFORM[@]}" =~ ${item} ]] ; then
 		FOUND_MATCH=1
 		echo "--> Found match of ${item} in ${_PLATFORM[@]} for ${BUILD_TYPE}"
 	    fi
